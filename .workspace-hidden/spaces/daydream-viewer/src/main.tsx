f3f3e0bd4d577bf649fe6992145d8165accc1083
Kit <kit@hatch.ws>
2026-06-21 00:08:49 +0000

    Add .workspace-hidden/ to .gitignore and remove from tracking

--- .workspace-hidden/spaces/daydream-viewer/src/main.tsx
+++
@@ -1,4 +0,0 @@
-import RenderSpace from "virtual:hatch-space-entry";
-import { bootGeneratedSpace } from "@hatch/sdk/bun";
-
-bootGeneratedSpace(RenderSpace);
--- .workspace-hidden/spaces/remote-browser/src/main.tsx
+++
@@ -1,5 +0,0 @@
-import RenderSpace from "virtual:hatch-space-entry";
-import { bootGeneratedSpace } from "@hatch/sdk/bun";
-import "./theme.css";
-
-bootGeneratedSpace(RenderSpace);
